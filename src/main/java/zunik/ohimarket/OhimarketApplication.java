829e70b3233f3feb347e2c1b6a32d19328c70a5a
zunik <chazunik@gmail.com>
2023-02-09 01:46:37 +0900

    first commit

---
+++ src/main/java/zunik/ohimarket/OhimarketApplication.java
@@ -0,0 +1,13 @@
+package zunik.ohimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhimarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhimarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zunik/ohimarket/OhimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package zunik.ohimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhimarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
