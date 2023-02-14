549d05bf84b35fbf94fdb2dac9df304e1b5fca5a
zunik <chazunik@gmail.com>
2023-02-14 17:08:45 +0900

    시간 경과 00분 전, 00초 전, 00일 전 등으로 표시

---
+++ src/main/java/zunik/ohimarket/ThymeleafUtils.java
@@ -0,0 +1,37 @@
+package zunik.ohimarket;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+import static java.time.temporal.ChronoUnit.*;
+
+/**
+ * thymeleaf 용 커스텀 util
+ */
+@Component
+public class ThymeleafUtils {
+    public String datetimeToStr(LocalDateTime dateTime) {
+        LocalDateTime now = LocalDateTime.now();
+
+        Long seconds = SECONDS.between(dateTime, now);
+
+        if (seconds < 60) {
+            return seconds + "초 전";
+        } else if (seconds < 3600) {
+            Long minutes = seconds / 60;
+            return minutes + "분 전";
+        } else if (seconds < 86400) {
+            Long hours = seconds / 3600;
+            return hours + "시간 전";
+        }
+
+        Long days = DAYS.between(dateTime, now);
+        if (days == 1) {
+            return "어제";
+        } else if (days == 2) {
+            return "그저께";
+        }
+        return days + "일 전";
+    }
+}
