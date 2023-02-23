9581c67c89a7591df9b002717327747ccd576b01
zunik <chazunik@gmail.com>
2023-02-23 21:52:09 +0900

    이미지 업로드와 디스플레이 부분 개선

--- src/main/java/zunik/ohimarket/controller/ImageController.java
+++ src/main/java/zunik/ohimarket/controller/ImageController.java
@@ -1,16 +1,28 @@
 package zunik.ohimarket.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import zunik.ohimarket.utils.ImgFileStore;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ImageController {
@@ -18,13 +30,27 @@ public class ImageController {
 
     @ResponseBody
     @GetMapping("/images/{imgName}")
-    public Resource downloadImage(@PathVariable String imgName) throws MalformedURLException {
-        return new UrlResource("file:" + imgFileStore.getFullPath(imgName));
-    }
+    public ResponseEntity<Resource> downloadImage(
+            @PathVariable String imgName,
+            @RequestParam(value = "isThumbnail", required = false, defaultValue = "false") boolean isThumbnail) throws IOException {
 
-    @ResponseBody
-    @GetMapping("/images/{imgName}/thumbnail")
-    public Resource downloadThumbnailImage(@PathVariable String imgName) throws MalformedURLException {
-        return new UrlResource("file:" + imgFileStore.getThumbnailFullPath(imgName));
+        String fullPath = imgFileStore.getFullPath(imgName);
+
+        if (isThumbnail) {
+            // thumbnail 용 이미지일 경우, path 변경
+            fullPath = imgFileStore.getThumbnailFullPath(imgName);
+        }
+
+        Resource resource = new FileSystemResource(fullPath);
+
+        if (!resource.exists()) {
+            return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+        }
+
+        HttpHeaders headers =  new HttpHeaders();
+        Path filePath = Paths.get(fullPath);
+        headers.add(HttpHeaders.CONTENT_TYPE, Files.probeContentType(filePath));
+
+        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
     }
 }
